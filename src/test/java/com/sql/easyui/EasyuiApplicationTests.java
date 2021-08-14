071f77cde9e0c354477b003e6794ef9b1f7fecb0
SONTI RAMBHAJAN - MFCWL <23256559@mahindra.com>
2021-08-14 20:30:02 +0530

    first commit

---
+++ src/test/java/com/sql/easyui/EasyuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sql.easyui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
